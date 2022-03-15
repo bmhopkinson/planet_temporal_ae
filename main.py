0fb09c9010ef0b534f50ea1ef31026dfdd27c4e3
Brian Hopkinson <bmhopkin@uga.edu>
2022-03-15 13:13:13 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
+#https://towardsdatascience.com/attention-seq2seq-with-pytorch-learning-to-invert-a-sequence-34faf4133e53
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
